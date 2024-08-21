dd5c8a660c0a866a520999b7a4ca9c249728ffea
Colton Dibbs <colton.dibbs@thrivent.com>
2024-08-21 12:09:46 -0500

    Basic LWC

--- force-app/main/default/lwc/todos/todos.js
+++ force-app/main/default/lwc/todos/todos.js
@@ -1,3 +1,9 @@
-import { LightningElement } from 'lwc';
+import {LightningElement} from 'lwc';
 
-export default class Todos extends LightningElement {}
+export default class Todos extends LightningElement {
+    todos = [
+        {Name: 'Do the dishes', DueDate__c: '2024-09-17', IsComplete__c: false},
+        {Name: 'Walk the dog', DueDate__c: '2024-08-23', IsComplete__c: false}
+    ];
+}
+
